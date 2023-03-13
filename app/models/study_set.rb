0161394843f2d591e157706a49b08317abe24f1e
HatAndBread <61222965+HatAndBread@users.noreply.github.com>
2023-03-13 13:12:02 +0900

    Allow less than three

--- app/models/study_set.rb
+++ app/models/study_set.rb
@@ -2,6 +2,6 @@ class StudySet < ApplicationRecord
   belongs_to :user
   has_many :mondais, dependent: :destroy
   accepts_nested_attributes_for :mondais, reject_if: ->(o) { o[:kanji].blank? || o[:yomikata.blank?] }, allow_destroy: true
-  validates :mondais, length: {minimum: 3, message: ": must be at least 3."}
+  # validates :mondais, length: {minimum: 3, message: ": must be at least 3."}
   validates :name, presence: true
 end
