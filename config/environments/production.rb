4f69ce00175f2cc473e86604347d01afec8a94ac
HatAndBread <61222965+HatAndBread@users.noreply.github.com>
2023-03-19 13:32:02 +0900

    Try caching public

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,11 +23,11 @@ Rails.application.configure do
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV["RAILS_SERVE_STATIC_FILES"].present?
-  
+
   # Added by me. Enable caching?
-  # config.public_file_server.headers = {
-  #   "Cache-Control" => "public, max-age=#{7.days.to_i}"
-  # }
+  config.public_file_server.headers = {
+    "Cache-Control" => "public, max-age=#{1.year.to_i}"
+  }
 
   # Compress CSS using a preprocessor.
   # config.assets.css_compressor = :sass
